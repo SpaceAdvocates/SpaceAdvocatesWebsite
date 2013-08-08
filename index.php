96498385b883df6f5599531f0899229b440aa59c
Joseph Spens <joseph@spens.us>
2013-08-08 09:34:16 -0400

    Moved theme to the root of the repo
    We don't need to keep the wordpress installation in the repository,
    just the theme. For new environments, download and install Wordpress
    from their website, update it, and then download and install the theme.

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php get_header(); ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+	<section id="main" role="main">
 
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
+		<?php $posts = get_posts(array(
+			'post_type' => 'post',
+			'post_per_page' => 5
+		));
+
+		if( count($posts) > 0 ): foreach( $posts as $post ): setup_postdata( $post ); ?>
+
+			<?php get_template_part( 'templates/partials/content', get_post_format() ); ?>
+
+		<?php endforeach; ?>
+
+		<?php get_template_part( 'templates/partials/inc', 'nav' ); ?>
+
+		<?php else : ?>
+
+			<article>
+				<h1>Not Found</h1>
+			</article>
+
+		<?php endif; ?>
+
+	</section> <!-- /#main -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
