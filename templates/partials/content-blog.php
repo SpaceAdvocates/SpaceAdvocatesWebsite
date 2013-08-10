76724b87120f5948a58ce645a427d179eb8dcd59
Rhettajf <design@rhettforbes.com>
2013-08-10 17:45:04 -0400

    Blog Roll Content Specific Headers
    Added support to check post type and style headers accordingly. Used
    category for videos and meta for featured.

--- templates/partials/content-blog.php
+++ templates/partials/content-blog.php
@@ -6,8 +6,17 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
-
+<?php
+if ( in_category( 'videos' )) {?>
+<a href="<?php the_permalink(); ?>"><h1 class="videos"><?php the_title(); ?></h1></a>
+<?php } elseif (  get_post_meta( get_the_ID(), '_cmb_featured_value', true ) ) {?>
+<a href="<?php the_permalink(); ?>"><h1 class="featured"><?php the_title(); ?></h1></a>
+<?php } else {?>
+<a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
+<?php } ?>
+<div class="row collapse">
+<?php the_post_thumbnail('full'); ?>
+</div>
 	<div class="entry">
 
 		<?php the_content('read&nbsp;more&nbsp;<span>&rarr;</span>', true); ?>
