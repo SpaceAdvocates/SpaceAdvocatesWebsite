84c9945c8a51e59d7ffa41f0fd591d98a6d71bc3
Rhettajf <design@rhettforbes.com>
2013-08-10 17:48:08 -0400

    Recent Posts thumbnail images
    Same problem as "Featured Thumbnail Images" rollback if problem is on my
    end only.

--- templates/partials/content-card.php
+++ templates/partials/content-card.php
@@ -5,11 +5,11 @@
 ?>
 
 <?php
-	$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );
-	$url = $thumb['0'];
+
+	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' );
 ?>
 <a href="<?php the_permalink(); ?>" class="small-12 medium-6 large-3 columns">
-	<div id="post-<?php the_ID(); ?>" class="card" style="background-image:url(<?= $url; ?>);">
+	<div id="post-<?php the_ID(); ?>" class="card" style="background-image:url(<?php echo $url; ?>);">
 		<h1><?php the_title(); ?></h1>
 		<?php //comments_popup_link(); ?>
 	</div>
