a69f1d90ed5e3f50a936fce2cf9e03e4953a0125
Joseph Spens <joseph@spens.us>
2013-08-25 21:06:21 -0400

    Reverted top stories images to background images
    Was changed to using img tags, but that makes each card a different
    height

--- templates/partials/content-featured.php
+++ templates/partials/content-featured.php
@@ -4,12 +4,15 @@
  */
 ?>
 
+<?php
+
+	$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'homepage-thumb' );
+	$url = $thumb['0'];
+
+?>
+
 <a href="<?php the_permalink(); ?>" class="small-12 medium-6 large-3 columns">
-	<div id="post-<?php the_ID(); ?>" class="card featured">
-	<?php if ( has_post_thumbnail() ) {
-		the_post_thumbnail('homepage-thumb');
-	} ?>
-	</div>
+	<div id="post-<?php the_ID(); ?>" class="card featured" style="background-image: url(<?= $url ?>)"></div>
 	<div class="drawer">
 		<h1><?php the_title(); ?></h1>
 	</div>
