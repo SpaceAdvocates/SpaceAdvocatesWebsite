ee7fb9e7b4e7858b12f171bd0f254fd45ba30de8
John Zeller <johnlzeller@gmail.com>
2013-08-26 00:42:19 -0700

    Added a new template for Volunteer which includes shortcode for the contact-form-7 wordpress plugin

---
+++ templates/volunteer.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Contact Page
+*/
+?>
+
+<?php get_header(); ?>
+
+<h1 class="page_header">Volunteer</h1>
+
+<?php echo do_shortcode( '[contact-form-7 id="2147" title="Volunteer"]' ); ?>
+
+<?php get_footer(); ?>
