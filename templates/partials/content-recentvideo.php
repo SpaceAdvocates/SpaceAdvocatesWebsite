0843e006f1f8832989db2beea9bf8f4fe924b018
Joseph Spens <joseph@spens.us>
2013-08-25 20:24:13 -0400

    Fixed a bug for recent video content
    When loading in the post content for recent videos, content HTML tags
    were screwing with my wrappers, so I used regex to isolate only the
    iframes and ignore the rest of the content.

--- templates/partials/content-recentvideo.php
+++ templates/partials/content-recentvideo.php
@@ -4,15 +4,18 @@
  */
 ?>
 
+<?php
+
+	preg_match('/(<iframe.*<\/iframe>)/', get_the_content(), $matches);
+	$video = $matches[0];
+
+?>
+
 <div class="small-12 medium-6 large-6 columns">
 	<a href="<?php the_permalink(); ?>">
 		<div id="post-<?php the_ID(); ?>" class="card recentvideo">
 			<div class="flex-video">
-				<?php
-					global $more;
-					$more = 0;
-				?>
-				<?php strip_tags(the_content('')); ?>
+				<?= $video ?>
 			</div>
 		</div>
 		<div class="drawer">
