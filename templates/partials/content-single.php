3593c8f8f7b0a01d71b72da2b22977e7be1c9175
Joseph Spens <joseph@spens.us>
2013-08-28 12:44:43 -0400

    Separated single content headers from blog post headers

--- templates/partials/content-single.php
+++ templates/partials/content-single.php
@@ -7,20 +7,9 @@
  */
 ?>
 
-<?php
-
-	$class = '';
-	if (  get_post_meta( get_the_ID(), '_cmb_featured_value', true ) )
-		$class = 'featured';
-
-?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<a href="<?php the_permalink(); ?>">
-		<h1 class="<?php echo $class; ?>"><?php the_title(); ?></h1>
-		<div class="star"></div>
-	</a>
+	<h1><?php the_title(); ?></h1>
 
 	<?php // get_template_part( 'templates/partials/inc', 'meta' ); ?>
 
