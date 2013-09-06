c78438fc8beaa83fe5fe198c4f6f09348566a7a3
Joseph Spens <jspens@spaceadvocates.com>
2013-09-06 02:50:52 -0400

    Added Press Room styles

--- templates/about.php
+++ templates/about.php
@@ -2,33 +2,13 @@
 /*
 Template Name: About Page
 */
-?>
 
-<?php get_header(); ?>
-	<section id="main" role="main" class="row">
-
-<h1>
-We are concerned citizens.<br />
-We are scientists.<br />
-We are students.<br />
-We are teachers.<br />
-We brought you Penny4NASA.<br />
-We are Space Advocates.
-</h1>
-
-
-<div id="mission">
-	<h2>our Mission</h2>
-	<p>Space Advocates is a nonprofit space advocacy organization focused on propelling us into the universe through mining our most precious asset: people. We call upon these space advocates to help us push the envelope, to speak louder, to become involved and put us at the forefront of space exploration.<br><br>
-	We are space advocates and <strong>together we will be heard.</strong></p>
-</div>
-
-<h2>The Crew</h2>
-
-<?php
 	$ranks = $wpdb->get_col("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'rank' GROUP BY meta_value ORDER BY umeta_id" );
 	sort ( $ranks );
-	foreach( $ranks as $rank ) { 
+
+	foreach( $ranks as $rank ) {
+		$sectionOutput = array();
+		
 		$people = get_users(array(
 			'meta_key' => 'rank',
 			'meta_value' => $rank
@@ -36,48 +16,65 @@ We are Space Advocates.
 
 		usort($people, 'sortPositions');
 
-		$title = ucwords(str_replace('_',' ',$rank));
-		if($rank == 'director') $title .= 's';
-		?>
-		
-		<section>
-			<h3><?php echo $title; ?></h3>
+		$sectionOutput['title'] = ucwords(str_replace('_',' ',$rank));
+		if($rank == 'director') $sectionOutput['title'] .= 's';
 
-			<?php
-			$count = 0;
-			foreach( $people as $person ) {
-				$count++;
+		$count = 0;
+		foreach( $people as $person ) {
+			$count++;
 
-				$name = $person->data->display_name;
-				$rank = esc_attr( get_the_author_meta( 'rank', $person->ID ) );
-				$position = esc_attr( get_the_author_meta( 'position', $person->ID ) );
-				$url = esc_attr( get_the_author_meta( 'url', $person->ID ) );
-				$user_info = get_userdata( $person->ID );
-				$avatar = get_avatar( $person->ID, 150 );
+			$user_info = get_userdata( $person->ID );
 
-				if ($count % 2 == 1 ): ?>
-				<div class="row">
-				<?php endif; ?>
-				
-				<div class="person columns large-6 small-12">
-					<?php echo $avatar; ?>
-					<h4 class="name">
-						<?php if($url): ?>
-							<a href="<?php echo $url; ?>"><?php echo $name; ?></a>
-						<?php else: ?>
-							<?php echo $name; ?>
-						<?php endif; ?>
-					</h4>
-					<h5 class="position"><?php echo $position; ?></h5>
-					<div class="bio"><?php echo $user_info->user_description; ?></div>
-				</div>
+			$sectionOutput['people'][] = array(
+				'name' => $person->data->display_name,
+				'position' => esc_attr( get_the_author_meta( 'position', $person->ID ) ),
+				'url' => esc_attr( get_the_author_meta( 'url', $person->ID ) ),
+				'bio' => $user_info->user_description,
+				'avatar' => get_avatar( $person->ID, 150 ),
+				'isEven' => ($count % 2 == 0 )
+			);
+		}
 
-				<?php if ($count % 2 == 0 ): ?>
-				</div>
-				<?php endif;
-			} ?>
-		</section>
-	<?php }
+		$output[] = $sectionOutput;
+	}
 ?>
-</section> <!-- /#main -->
+
+<?php get_header(); ?>
+
+<h1>We are concerned citizens.<br />We are scientists.<br />We are students.<br />We are teachers.<br />We brought you Penny4NASA.<br />We are Space Advocates.</h1>
+
+<div id="mission">
+	<h2>Our Mission</h2>
+	<p>Space Advocates is a nonprofit space advocacy organization focused on propelling us into the universe through mining our most precious asset: people. We call upon these space advocates to help us push the envelope, to speak louder, to become involved and put us at the forefront of space exploration.</p>
+	<p>We are space advocates and <strong>together we will be heard.</strong></p>
+</div>
+
+<h2>The Crew</h2>
+
+<?php foreach( $output as $section ) : ?>
+	<section>
+		<h3><?= $section['title']; ?></h3>
+		<?php foreach( $section['people'] as $person ) : ?>
+			<?php if (!$person['isEven']) : ?>
+				<div class="row">
+			<?php endif; ?>
+			<div class="person columns large-6 small-12">
+				<?= $person['avatar']; ?>
+				<h4 class="name">
+					<?php if($person['url']): ?>
+						<a href="<?= $person['url']; ?>"><?= $person['name']; ?></a>
+					<?php else: ?>
+						<?php echo $person['name']; ?>
+					<?php endif; ?>
+				</h4>
+				<h5 class="position"><?= $peron['position']; ?></h5>
+				<div class="bio"><?= $person['bio']; ?></div>
+			</div>
+			<?php if ($person['isEven']) : ?>
+				</div>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	</section>
+<?php endforeach; ?>
+
 <?php get_footer(); ?>
--- templates/press.php
+++ templates/press.php
@@ -7,36 +7,102 @@ Template Name: Press Page
 
 <?php get_header(); ?>
 
-<center><h1 class="page_header">Press Room</h1></center>
+<h1 class="page_header">Press Room</h1>
+<div class="row">
+	<div class="large-5 columns press-kit">
+		<a class="btn-red" href="javascript:void(0)">Download Press Kit</a>
+	</div>
+</div>
 
-<div class="row"><div class="small-12 tablet-12 large-9 large-centered columns" style="color:#fff;" id="contact_form">
-<table style="width:100%;border:none;background:transparent;color:#fff;">
-<tr style="background:transparent;"><td colspan="2"><h3>Media Contact</h3></td></tr>
-<tr style="background:transparent;"><td><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/Sumedha-268x300.jpg" alt="Sumedha" width="268" height="300" class="alignnone size-medium wp-image-2096" /></td>
-<td>
-<h4>Director of Public and Media Relations</h4>
-<h5>Sumedha Garud</h5>
-<p style="color:#fff;">sumedha@spaceadvocates.com<br>
-424.236.2787</p></td></tr>
-<tr style="background:transparent;"><td><br><br><a class="btn-red" href="javascript:void(0)">Download Press Kit</a></td></tr>
+<h2>Media Contact</h2>
+<div class="row contact">
+	<div class="large-4 columns">
+		<div class="person">
+			<img src="<?php bloginfo('template_directory'); ?>/assets/images/Sumedha.png" alt="Sumedha" width="315" height="315" />
+		</div>
+	</div>
+	<div class="large-8 columns info">
+		<h3>Sumedha Garud</h3>
+		<h4>Director of Public and Media Relations</h4>
+		<p>sumedha@spaceadvocates.com</p>
+		<p>424.236.2787</p>
+	</div>
+</div>
 
-<tr style="background:transparent;"><td colspan="2"><h3>Press Releases</h3><p style="color:#fff;">
-<a title="3/12/2012 CITIZEN MOVEMENT SEEKS INCREASE TO NASA’S BUDGET" href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/PressRelease1.pdf">3/12/2012 CITIZEN MOVEMENT SEEKS INCREASE TO NASA’S BUDGET</a><br>
-... We know, this is a bit bare, but there are more to come!</p></td></tr>
+<div class="row">
+	<div class="large-6 columns">
+		<div class="press-section">
+			<h3>Press Releases</h3>
+			<div class="content">
+				<ul>
+					<li><a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/PressRelease1.pdf">3/12/2012 CITIZEN MOVEMENT SEEKS INCREASE TO NASA’S BUDGET</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="large-6 columns">
+		<div class="press-section">
+			<h3>In The News</h3>
+			<div class="content">
+				<ul>
+					<li><a href="http://gizmodo.com/5962595/why-the-government-must-spend-more-money-on-nasa">Gizmodo.com - "Why the Government Must Spend More Money On NASA" by Jesus Diaz, November 26, 2012</a></li>
+					<li><a href="http://www.slate.com/blogs/future_tense/2012/06">Slate.com / Future Tense Blog - "Neil deGrasse Tyson: Why We Need To Double NASA’s Budget" by Torie Bosch, June 4, 2012</a></li>
+					<li><a href="http://www.washingtonpost.com/blogs/innovations/post/neil-degrasse-tyson-asks-us-how-much-the-universe-is-worth-the-web-responds/2012/03/19/gIQAvw34NS_blog.html">WashingtonPost.com / Innovations Blog - "Neil DeGrasse Tyson asks us how much the universe is worth; the Web responds" by Emi Kolawole, March 20, 2012</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
 
-<tr style="background:transparent;"><td colspan="2"><h3>In The News</h3>
-<p style="color:#fff;"><a title="Gizmodo.com - 'Why the Government Must Spend More Money On NASA' by Jesus Diaz, November 26, 2012" href="http://gizmodo.com/5962595/why-the-government-must-spend-more-money-on-nasa">Gizmodo.com - "Why the Government Must Spend More Money On NASA" by Jesus Diaz, November 26, 2012</a><br>
-<a title="Slate.com / Future Tense Blog - 'Neil deGrasse Tyson: Why We Need To Double NASA’s Budget' by Torie Bosch, June 4, 2012" href="http://www.slate.com/blogs/future_tense/2012/06">Slate.com / Future Tense Blog - "Neil deGrasse Tyson: Why We Need To Double NASA’s Budget" by Torie Bosch, June 4, 2012</a><br>
-<a title="WashingtonPost.com / Innovations Blog - 'Neil DeGrasse Tyson asks us how much the universe is worth; the Web responds' by Emi Kolawole, March 20, 2012" href="http://www.washingtonpost.com/blogs/innovations/post/neil-degrasse-tyson-asks-us-how-much-the-universe-is-worth-the-web-responds/2012/03/19/gIQAvw34NS_blog.html">WashingtonPost.com / Innovations Blog - "Neil DeGrasse Tyson asks us how much the universe is worth; the Web responds" by Emi Kolawole, March 20, 2012</a></p></td></tr>
-<tr style="background:transparent;"><td colspan="2"><h3>Space Advocates Logos</h3></td></tr>
-<tr style="background:transparent;"><td colspan="2"><a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/Space-Advocates-RGB-PNG.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/Space-Advocates-RGB-PNG.png" alt="Space Advocates Logo" width="150" height="150" class="alignnone size-full wp-image-2077" /></a><a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/SA_logo_astronaut.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/SA_logo_astronaut.png" alt="Space Advocates Advocate" width="150" height="150" class="alignnone size-thumbnail wp-image-2078" /></a></td></tr>
-<tr style="background:transparent;"><td colspan="2"><h3>Penny4NASA Logos</h3></td></tr>
-<tr style="background:transparent;"><td colspan="2"><a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/ICON.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/ICON.png" alt="Penny4NASA Logo" width="150" height="150" class="alignnone size-thumbnail wp-image-2079" /></a><a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-VERTICAL-NEW.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-VERTICAL-NEW.png" alt="Penny4NASA - Vertical" width="150" height="150" class="alignnone size-thumbnail wp-image-2080" /></a><a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-HORIZONTAL-NEW.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-HORIZONTAL-NEW.png" alt="Penny4NASA - Horizontal" width="150" height="150" class="alignnone size-thumbnail wp-image-2081" /></a></td></tr>
-<tr style="background:transparent;"><td colspan="2"><h3>Other Space Advocates Images</h3></td></tr>
-<tr style="background:transparent;"><td colspan="2"><p style="color:#fff;">These are images that our team has designed and use accross each of our social media platforms. These particular images are being pulled from our <a href="http://www.facebook.com/PennyForNASA">Penny4NASA facebook page</a>.</p>
+<div class="row">
+	<div class="large-6 columns">
+		<div class="press-section">
+			<h3>Space Advocates Logos</h3>
+			<div class="content">
+				<div class="row">
+					<div class="large-6 columns">
+						<a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/Space-Advocates-RGB-PNG.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/Space-Advocates-RGB-PNG.png" alt="Space Advocates Logo" width="150" height="150" class="alignnone size-full wp-image-2077" /></a>
+					</div>
+					<div class="large-6 columns">
+						<a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/SA_logo_astronaut.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/SA_logo_astronaut.png" alt="Space Advocates Advocate" width="150" height="150" class="alignnone size-thumbnail wp-image-2078" /></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="large-6 columns">
+		<div class="press-section">
+			<h3>Penny4NASA Logos</h3>
+			<div class="content">
+				<div class="row">
+					<div class="large-4 columns">
+						<a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/ICON.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/ICON.png" alt="Penny4NASA Logo" width="150" height="150" class="alignnone size-thumbnail wp-image-2079" /></a>
+					</div>
+					<div class="large-4 columns">
+						<a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-VERTICAL-NEW.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-VERTICAL-NEW.png" alt="Penny4NASA - Vertical" width="150" height="150" class="alignnone size-thumbnail wp-image-2080" /></a>
+					</div>
+					<div class="large-4 columns">
+						<a href="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-HORIZONTAL-NEW.png"><img src="http://dev1.spaceadvocates.com/wp-content/uploads/2013/08/LOGO-HORIZONTAL-NEW.png" alt="Penny4NASA - Horizontal" width="150" height="150" class="alignnone size-thumbnail wp-image-2081" /></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-<?php echo do_shortcode( '[fbalbum url=https://www.facebook.com/media/set/?set=a.371680972862823.88424.370002089697378&type=3]' ); ?></td></tr>
-</table>
-</div></div>
+<div class="row">
+	<div class="large-12 columns">
+		<div class="press-section">
+			<h3>Other Space Advocates Images</a></h3>
+			<div class="content">
+				<div class="row">
+					<div class="large-12 columns">
+						<a href="https://www.facebook.com/media/set/?set=a.371680972862823.88424.370002089697378&amp;type=3">Visit our Facebook album</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
