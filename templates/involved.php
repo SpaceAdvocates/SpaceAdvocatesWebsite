4ac6631063ebb544d3b8250ef61ed6d9b67c8963
Joseph Spens <joseph@spens.us>
2013-09-05 23:50:34 -0400

    Updated Get Involved page to include social media

--- templates/involved.php
+++ templates/involved.php
@@ -41,31 +41,35 @@ Template Name: Involved Page
 		</div>
 	</div>
 </div>
-<div class="involvement-step">
+<div class="involvement-step social">
 	<div class="row">
 		<span class="number">4</span>
-		<p>Like, follow, 1+ and subscribe to all of our social media pages!</p>
-		<div class="small-3 columns">
-			<a class="facebook" href="https://www.facebook.com/SpaceAdvocates"></a>
+		<div class="large-4 large-offset-1 columns">
+			<div class="row">
+				<div class="small-3 large-4 columns social">
+					<div class="fb-like" data-href="https://www.facebook.com/PennyForNASA" data-width="350" data-layout="button_count" data-show-faces="false" data-send="false"></div>
+				</div>
+				<div class="small-3 large-8 columns social">
+					<a href="https://twitter.com/SpaceAdvocates" class="twitter-follow-button" data-show-count="false">Follow @SpaceAdvocates</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-3 large-5 columns social">
+					<div class="g-follow" data-annotation="bubble" data-height="24" data-href="https://plus.google.com/116044676098904697251" data-rel="publisher"></div>
+				</div>
+				<div class="small-3 large-7 columns social">
+					<div class="g-ytsubscribe" data-channel="SpaceAdvocates" data-layout="default"></div>
+				</div>
+			</div>
 		</div>
-		<div class="small-3 columns">
-			<a class="google" href="https://plus.google.com/b/116044676098904697251/116044676098904697251"></a>
-		</div>
-		<div class="small-3 columns">
-			<a class="twitter" href="https://twitter.com/SpaceAdvocates"></a>
-		</div>
-		<div class="small-3 columns">
-			<a class="youtube" href="http://www.youtube.com/SpaceAdvocates"></a>
+		<div class="large-7 columns">
+			<p>Like, follow, and subscribe to all of our social media pages!</p>
 		</div>
 	</div>
 </div>
 <div class="involvement-step">
-	<span class="number">5</span>
-	<p>Educate Yourself by reading through our <a href="/faq">FAQ</a> page and checking out video on our <a href="http://www.youtube.com/SpaceAdvocates">YouTube channel</a>.</p>
-</div>
-<div class="involvement-step">
 	<div class="row">
-		<span class="number">6</span>
+		<span class="number">5</span>
 		<div class="large-4 large-offset-1 columns">
 			<a href="/volunteer" class="cta btn-red">Volunteer</a>
 		</div>
@@ -73,6 +77,9 @@ Template Name: Involved Page
 			<p>If you have enough interest that you have reached this step and are still wanting to do more, then fill out our volunteer form here!</p>
 		</div>
 	</div>
+</div>
+<div class="involvement-step">
+	<p>Educate Yourself by reading through our <a href="/faq">FAQ</a> page and checking out video on our <a href="http://www.youtube.com/SpaceAdvocates">YouTube channel</a>.</p>
 </div>
 
 <?php get_footer(); ?>
