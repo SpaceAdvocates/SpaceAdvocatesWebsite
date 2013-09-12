4e3e7130c0b7ef23b2f4b7f755f21f4c8acf9185
Joseph Spens <jspens@spaceadvocates.com>
2013-09-12 15:46:36 -0400

    Added styles to volunteer form

--- templates/volunteer.php
+++ templates/volunteer.php
@@ -8,17 +8,9 @@ Template Name: Volunteer Page
 
 <div class="small-12 columns">
 	<h1>Volunteer</h1>
-
-	<p>Space Advocates is a social advocacy organization concerned with the improvement of, and support for, space exploration in every form. We have many people already on our team and we are constantly growing. If you are interested in helping out then shoot us an email on the following form and check the box for the team you are interested in joining!</p>
-	<p>We currently aren’t looking for specific positions, but when we are, we will post a description of the ideal candidates here! :)</p>
-
-	<div class="row">
-		<div class="small-12 large-9 columns" id="volunteer_form">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; ?>
-		</div>
-	</div>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; ?>
 </div>
 
 <?php get_footer(); ?>
